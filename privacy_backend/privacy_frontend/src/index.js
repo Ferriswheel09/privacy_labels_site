5ed3d0df06e848e2cd7e341bd98c4c4714c43971
Ferriswheel09 <fjiwad@gwu.edu>
2023-05-31 12:28:05 -0400

    Fixed some issues with merge

--- privacy_backend/privacy_frontend/src/index.js
+++ privacy_backend/privacy_frontend/src/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Header from "./Frontend";
+import { Header } from "./Frontend";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/boostrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header/>
+    <Header />
   </React.StrictMode>
 );
 
