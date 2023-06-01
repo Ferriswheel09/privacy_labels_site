8092ee7ff4f5c4f6d959b28786abb7efca7cdfcf
Ferriswheel09 <fjiwad@gwu.edu>
2023-06-01 14:24:41 -0400

    Added conversion methods for letters to strings

--- privacy_site/privacy_frontend/src/components/table.js
+++ privacy_site/privacy_frontend/src/components/table.js
@@ -16,6 +16,179 @@ class Table extends Component {
     });
   }
 
+  privacyTypeMethod(str) {
+    //("NC", "Data Not Collected"),
+    //("NL", "Data Not Linked To You"),
+    //("L", "Data Linked To You"),
+    //("T", "Data Used To Track You"),
+    if (str === "NC") {
+      return "Data Not Collected";
+    }
+    if (str === "NL") {
+      return "Data Not Linked To You";
+    }
+    if (str === "L") {
+      return "Data Linked To You";
+    }
+    return "Data Used To Track You";
+  }
+
+  purposeMethod(str) {
+    //("A", "Analytics"),
+    //("AP", "App Functionality"),
+    //("D", "Developer's Advertising or Marketing"),
+    //("O", "Other Purposes"),
+    //("P", "Product Personalization"),
+    //("T", "Third-party Advertising"),
+    if (str === "A") {
+      return "Analytics";
+    }
+    if (str === "AP") {
+      return "App Functionality";
+    }
+    if (str === "D") {
+      return "Developer's Advertising or Marketing";
+    }
+    if (str === "P") {
+      return "Product Personalization";
+    }
+    if (str === "T") {
+      return "Third-party Advertising";
+    }
+    return "Other Purposes";
+  }
+
+  dataCategoryMethod(str) {
+    if (str === "B") {
+      return "Browsing History";
+    }
+    if (str === "CI") {
+      return "Contact Info";
+    }
+    if (str === "C") {
+      return "Contacts";
+    }
+    if (str === "D") {
+      return "Diagnostics";
+    }
+    if (str === "F") {
+      return "Financial Info";
+    }
+    if (str === "H") {
+      return "Health and Fitness";
+    }
+    if (str === "I") {
+      return "Identifiers";
+    }
+    if (str === "L") {
+      return "Location Info";
+    }
+    if (str === "O") {
+      return "Other Data";
+    }
+    if (str === "P") {
+      return "Purchases";
+    }
+    if (str === "SH") {
+      return "Search History";
+    }
+    if (str === "SI") {
+      return "Sensitive Info";
+    }
+    if (str === "UD") {
+      return "Usage Data";
+    }
+    if (str === "UC") {
+      return "User Content";
+    }
+    return "Unknown";
+  }
+
+  dataTypeMethod(str) {
+    if (str === "AD") {
+      return "Advertising Data";
+    }
+    if (str === "AU") {
+      return "Audio Data";
+    }
+    if (str === "B") {
+      return "Browsing History";
+    }
+    if (str === "CL") {
+      return "Coarse Location";
+    }
+    if (str === "C") {
+      return "Contacts";
+    }
+    if (str === "CD") {
+      return "Crash Data";
+    }
+    if (str === "CI") {
+      return "Credit Info";
+    }
+    if (str === "CS") {
+      return "Customer Support";
+    }
+    if (str === "D") {
+      return "Device ID";
+    }
+    if (str === "E") {
+      return "Email Address";
+    }
+    if (str === "ET") {
+      return "Emails or Text Messages";
+    }
+    if (str === "F") {
+      return "Fitness";
+    }
+    if (str === "G") {
+      return "Gameplay Content";
+    }
+    if (str === "H") {
+      return "Health";
+    }
+    if (str === "N") {
+      return "Name";
+    }
+    if (str === "PI") {
+      return "Payment Info";
+    }
+    if (str === "PD") {
+      return "Performance Data";
+    }
+    if (str === "PN") {
+      return "Phone Number";
+    }
+    if (str === "PV") {
+      return "Photos or Videos";
+    }
+    if (str === "PA") {
+      return "Physical Address";
+    }
+    if (str === "PL") {
+      return "Precise Location";
+    }
+    if (str === "PR") {
+      return "Product Interaction";
+    }
+    if (str === "PH") {
+      return "Purchase History";
+    }
+    if (str === "SH") {
+      return "Search History";
+    }
+    if (str === "SI") {
+      return "Sensitive Info";
+    }
+    if (str === "U") {
+      return "User ID";
+    }
+    if (str === "O") {
+      return "Other";
+    }
+    return "Unknown";
+  }
+
   render() {
     return (
       <div>
@@ -38,10 +211,10 @@ class Table extends Component {
               <tr key={item.id}>
                 <th scope="row">{item.id}</th>
                 <td>{item.app_name}</td>
-                <td>{item.privacy_type}</td>
-                <td>{item.purpose}</td>
-                <td>{item.data_category}</td>
-                <td>{item.data_type}</td>
+                <td>{this.privacyTypeMethod(item.privacy_type)}</td>
+                <td>{this.purposeMethod(item.purpose)}</td>
+                <td>{this.dataCategoryMethod(item.data_category)}</td>
+                <td>{this.dataTypeMethod(item.data_type)}</td>
                 <td>{item.release_date}</td>
                 <td>{item.version_release_date}</td>
                 <td>{item.app_url}</td>
